13baa20e76849c6f25583cdd86397ef4c90e9b25
yqxd <xuz19556@gmail.com>
2020-08-24 10:48:48 +0800

    3

--- 102BinaryTreeLevelOrderTraversal.cpp
+++ 102BinaryTreeLevelOrderTraversal.cpp
@@ -15,21 +15,23 @@
 //   [15,7]
 // ]
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
 #include <stdio.h>
 #include <vector>
+using namespace std;
 class Solution {
    public:
-    vector<vector<int>> levelOrder(TreeNode* root) {}
+    vector<vector<int> > levelOrder(TreeNode* root) {
+        vector<vector<int> > result;
+        return result;
+    }
 };
