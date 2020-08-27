2fd4dfcd37d26d3425cb7a88ccd70c01c9baf547
yqxd <xuz19556@gmail.com>
2020-08-27 11:05:24 +0800

    2

---
+++ debug.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+int main() {
+    struct TreeNode a1, a2, a3, a4, a5, a6, *b1 = &a1, *b2 = &a2, *b3 = &a3,
+                                            *b4 = &a4, *b5 = &a5, *b6 = &a6;
+    a1.val = 1;
+    a2.val = 2;
+    a3.val = 3;
+    a4.val = 4;
+    a5.val = 5;
+    a6.val = 6;
+    b1->left = b2;
+    b1->right = b3;
+    b2->left = b4;
+    b2->right = b5;
+    b5->left = b6;
+    vector<struct TreeNode*> T;
+    T.push_back(b1);
+    struct TreeNode* N = T[T.size() - 1];
+    cout << N->val;
+    cout << T.size();
+}
