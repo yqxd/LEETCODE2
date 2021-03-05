6dd51737c8c31d2ebbb508005a017d8ba350c2e8
yqxd <xuz19556@gmail.com>
2021-03-05 14:38:08 +0800

    130

--- 130SurroundedRegions.cpp
+++ 130SurroundedRegions.cpp
@@ -24,30 +24,103 @@
 // 'X'. Two cells are connected if they are adjacent cells connected
 // horizontally or vertically.
 
-#include <math.h>
+#include <cmath>
 #include <stdio.h>
 #include <algorithm>
 #include <iostream>
 #include <stack>
 #include <unordered_map>
 #include <vector>
+
 using namespace std;
 
 class Solution {
-   public:
-    void solve(vector<vector<char>>& board) {
+public:
+    void solve(vector<vector<char>> &board) {
         if (board.size() < 3 || board[0].size() < 3) {
             return;
-        }
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board.size(); j++){
-
+        } else {
+            vector<vector<int>> stack;
+            for (int i = 0; i < board[0].size(); i++) {
+                if (board[0][i] == 'O') {
+                    stack.push_back({0, i});
+                }
+                if (board[board.size() - 1][i] == 'O') {
+                    stack.push_back({static_cast<int>(board.size() - 1), i});
+                }
+            }
+            for (int i = 1; i < board.size() - 1; i++) {
+                if (board[i][0] == 'O') {
+                    stack.push_back({i, 0});
+                }
+                if (board[i][board[0].size() - 1] == 'O') {
+                    stack.push_back({i, static_cast<int>((board[0].size() - 1))});
+                }
+            }
+            while (not stack.empty()) {
+                vector<int> M = stack[stack.size() - 1];
+                stack.pop_back();
+                board[M[0]][M[1]] = 'E';
+                if (M[0] > 0) {
+                    if (board[M[0] - 1][M[1]] == 'O') {
+                        stack.push_back({static_cast<int>(M[0] - 1), M[1]});
+                    }
+                }
+                if (M[0] < board.size() - 1) {
+                    if (board[M[0] + 1][M[1]] == 'O') {
+                        stack.push_back({static_cast<int>(M[0] + 1), M[1]});
+                    }
+                }
+                if (M[1] > 0) {
+                    if (board[M[0]][M[1] - 1] == 'O') {
+                        stack.push_back({static_cast<int>(M[0]), M[1] - 1});
+                    }
+                }
+                if (M[1] < board[0].size() - 1) {
+                    if (board[M[0]][M[1] + 1] == 'O') {
+                        stack.push_back({static_cast<int>(M[0]), M[1] + 1});
+                    }
+                }
+            }
+            for (int i = 0; i < board.size(); i++) {
+                for (int j = 0; j < board[0].size(); j++) {
+                    if (board[i][j] == 'O') {
+                        board[i][j] = 'X';
+                    } else if (board[i][j] == 'E') {
+                        board[i][j] = 'O';
+                    }
+                }
             }
         }
     }
-    bool check(vector<vector<char>>& board, int i, int j, int k){
-        if(i == 0 || j == 0 || i == board.size() - 1 || j == board[0].size() - 1){
-            return false;
-        } 
-    }
 };
+
+int main() {
+    char A[4][6] = {{'X', 'O', 'X', 'O', 'X', 'O'},
+                    {'O', 'X', 'O', 'X', 'O', 'X'},
+                    {'X', 'O', 'X', 'O', 'X', 'O'},
+                    {'O', 'X', 'O', 'X', 'O', 'X'}};
+    vector<vector<char>> B;
+    for (auto &i : A) {
+        B.emplace_back();
+        for (char j : i) {
+            B[B.size() - 1].push_back(j);
+        }
+    }
+
+    Solution C = Solution();
+    for (auto &i : B) {
+        cout << '\n';
+        for (char j : i) {
+            cout << j;
+        }
+    }
+    C.solve(B);
+    cout << '\n';
+    for (auto &i : B) {
+        cout << '\n';
+        for (char j : i) {
+            cout << j;
+        }
+    }
+}
